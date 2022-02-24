b582377f504a9d88f1100fae90e24635fb45dad1
josegarrera <josegarrera@gmail.com>
2022-02-24 11:47:11 -0300

    posts manager

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
+import {graphql, Link} from 'gatsby';
 import * as React from 'react';
 import Layout from '../components/Layout';
 import Seo from '../components/SEO';
 
-const IndexPage = () => {
+const IndexPage = ({data}) => {
+	const posts = data.allMdx.nodes;
 	return (
 		<Layout>
 			<Seo title={`Home Page`} />
-			<div>Hellow World</div>
+			{posts.map((post) => {
+				return (
+					<Link to={post.slug} key={post.slug}>
+						<h2>{post.frontmatter.title}</h2>
+					</Link>
+				);
+			})}
 		</Layout>
 	);
 };
+
+export const pageQuery = graphql`
+	{
+		allMdx(sort: {fields: [frontmatter___title], order: ASC}) {
+			nodes {
+				slug
+				frontmatter {
+					title
+				}
+			}
+		}
+	}
+`;
 
 export default IndexPage;
