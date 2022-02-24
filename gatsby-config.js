6212649e4c0bc90cfcd8e6aed2b4da6c3f5eee2e
josegarrera <josegarrera@gmail.com>
2022-02-24 11:20:50 -0300

    SEO configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
 module.exports = {
-    siteMetadata: {
-      title: ``,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+	siteMetadata: {
+		title: `Developer's Blog`,
+		description: `Jose Garrera developer's blog`,
+		twitter: `@josegarrera`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/posts`,
+				name: `posts`,
+			},
+		},
+		`gatsby-plugin-mdx`,
+	],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import Layout from '../components/Layout';
+import Seo from '../components/SEO';
 
 const IndexPage = () => {
 	return (
 		<Layout>
+			<Seo title={`Home Page`} />
 			<div>Hellow World</div>
 		</Layout>
 	);
